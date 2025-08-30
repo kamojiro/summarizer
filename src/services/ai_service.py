763f8279527e611ef4c52a2f64a1a0a18edba352
kamojiro <tamagoma002@yahoo.co.jp>
2025-08-30 22:44:24 +0900

    split message

--- src/services/ai_service.py
+++ src/services/ai_service.py
@@ -13,6 +13,7 @@ from google.genai.types import (
     Tool,
     UrlContext,
 )
+from numpy import append
 from pydantic import BaseModel
 
 load_dotenv()
@@ -32,10 +33,12 @@ SYSTEM_PROMPT = """あなたは有能なアシスタントです。
 - URL が含まれていない場合は、検索ツールを使用して情報を取得し、回答を生成してください
 - 分からなければとりあえず検索してください
 - 回答には、謝罪を含めないでください。
-- 文頭にタイトルをつけてください。
+- 文頭にタイトルをつけてください。タイトルは ** で囲んでください。# はつけないでください。
 
 [参考情報]
 """
+
+MISSKEY_MAX_LENGTH = 3000 - 50  # 余裕を持たせるために50文字引く
 
 
 class URLAccessError(Exception):
@@ -107,17 +110,34 @@ class AIService:
                 return False
         return True
 
-    def _generate_message(self, response: GenerateContentResponse) -> str:
+    def _split_text(self, text: str, references: list[str]) -> list[str]:
+        res = [
+            text[i : i + MISSKEY_MAX_LENGTH]
+            for i in range(0, len(text), MISSKEY_MAX_LENGTH)
+        ]
+        fragment = "\n**References**:"
+        for reference in references:
+            if len(fragment) + len(reference) + 1 > MISSKEY_MAX_LENGTH:
+                res.append(fragment)
+                fragment = reference
+            else:
+                fragment += "\n" + reference
+        res.append(fragment)
+        if (joined := "\n".join(res)) and len(joined) <= MISSKEY_MAX_LENGTH:
+            return [joined]
+        return res
+
+    def _generate_message(self, response: GenerateContentResponse) -> list[str]:
         if not response.text:
             raise ValueError("Response text is empty")
         text = response.text
         references = self._get_references(response)
-        reference = "\n".join([f"- [{ref.title}]({ref.uri})" for ref in references])
-        if reference:
-            text += "\n\n[References]\n" + reference
-        return text
+        formatted_references = [f"- [{ref.title}]({ref.uri})" for ref in references]
+        return self._split_text(text, formatted_references)
 
-    def generate_content(self, prompt: str, tool: Literal["search", "url"]) -> str:
+    def generate_content(
+        self, prompt: str, tool: Literal["search", "url"]
+    ) -> list[str]:
         config = self._get_config(tool)
         response: GenerateContentResponse = self.client.models.generate_content(
             model=self.model, contents=prompt, config=config
@@ -128,7 +148,7 @@ class AIService:
             )
         return self._generate_message(response)
 
-    def generate_content_if(self, prompt) -> str:
+    def generate_content_if(self, prompt) -> list[str]:
         tool: Literal["search", "url"] = "search"
         if "https://" in prompt:
             tool = "url"
--- src/services/misskey_service.py
+++ src/services/misskey_service.py
@@ -1,7 +1,9 @@
 import os
+from http import HTTPStatus
 
 import requests
 from dotenv import load_dotenv
+from fastapi import HTTPException
 from langchain_community.document_loaders import WebBaseLoader
 from vertexai.generative_models import GenerationConfig, GenerativeModel, Tool
 
@@ -26,21 +28,38 @@ class MisskeyService:
         self.missky_host = os.getenv("MISSKY_HOST")
         self.missky_token = os.getenv("MISSKY_TOKEN")
         self.ai_searvice = AIService()
+
     async def message_summaries(self, messages):
         for message in messages:
+            print("message:", message.content.strip())
             await self.message_summary(message.content.strip())
         return messages
 
-    async def message_summary(self, message):
-        text = self.ai_searvice.generate_content_if(message)
-
+    async def _post_to_misskey(self, body: dict[str, str]):
         headers = {"Content-Type": "application/json"}
         kamomai_url = f"https://{self.missky_host}/api"
-        body = {
-            "i": self.missky_token,
-            "visibility": "followers",
-            "text": text,
-        }
-        requests.post(
+        response = requests.post(
             f"{kamomai_url}/notes/create", headers=headers, json=body, timeout=5
         )
+        if response.status_code != HTTPStatus.OK:
+            raise ValueError(
+                "Error posting to Misskey:", response.status_code, response.text
+            )
+        return response.json()["createdNote"]["id"]
+
+    async def message_summary(self, message):
+        texts: list[str] = self.ai_searvice.generate_content_if(message)
+        body = {
+            "i": self.missky_token,
+            "visibility": "home",
+        }
+        message_id: str | None = None
+        for text in texts:
+            body["text"] = text
+            if message_id:
+                body["replyId"] = message_id
+            try:
+                message_id = await self._post_to_misskey(body)
+            except Exception as e:
+                print("Error posting to Misskey:", text)
+                raise e
